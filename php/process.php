0dea93f20d0f717032308a4f6149320f6d7a1376
Er-Praveen <rpraveenbkumar@gmail.com>
2020-06-22 18:38:35 +0530

    Initial commit

---
+++ php/process.php
@@ -0,0 +1,13 @@
+<?php
+ if (isset($_POST("submit"))){
+
+    // $to = "edwin@codingfaculty.com";
+    // $subject = $_POST('subject');
+    // $email = $_POST('email');
+    // $txt = $_POST('message');
+    // $headers = "Form: " .$email . "\r\n" . 
+    // "CC: example@eg.com";
+    // mail($to, $subject, $txt, $headers); 
+    // header("Location:contact.html");
+    echo "working";
+ } 
